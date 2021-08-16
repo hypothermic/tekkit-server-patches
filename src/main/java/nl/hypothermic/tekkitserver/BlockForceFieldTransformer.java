b82eaa33e92d05a804bde59607955dd9be788a9f
hypothermic <admin@hypothermic.nl>
2021-08-16 12:55:53 +0200

    Cleanup code in BlockForceFieldTransformer
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- src/main/java/nl/hypothermic/tekkitserver/BlockForceFieldTransformer.java
+++ src/main/java/nl/hypothermic/tekkitserver/BlockForceFieldTransformer.java
@@ -7,7 +7,34 @@ import org.objectweb.asm.tree.*;
 
 import static org.objectweb.asm.Opcodes.*;
 
-@MethodTransformer(targetClass = "mffs/BlockForceField",
+/**
+ * This transformer modifies "void a(World var1, int var2, int var3, int var4, Entity var5)"
+ *
+ * Original instructions:
+ *
+ * ```java
+ * if (!var1.isStatic && isZapper(var1.getData(var2, var3, var4)) && var5 instanceof EntityLiving) {
+ *     var5.damageEntity(DamageSource.GENERIC, 5);
+ * }
+ * ```
+ *
+ * New instructions:
+ *
+ * ```java
+ * if (!var1.isStatic && isZapper(var1.getData(var2, var3, var4)) && var5 instanceof EntityLiving) {
+ *     EntityPlayer fake = new EntityPlayer(((CraftServer)Bukkit.getServer()).getServer(), var1, "[MFFS]", new ItemInWorldManager(var1));
+ *     EntityDamageByEntityEvent event = (EntityDamageByEntityEvent)CraftEventFactory.callEntityDamageEvent(fake, var5, DamageCause.CUSTOM, 5);
+ *     if (!event.isCancelled()) {
+ *         var5.damageEntity(DamageSource.GENERIC, event.getDamage());
+ *         return;
+ *     } else {
+ *         return;
+ *     }
+ * }
+ * ```
+ */
+@MethodTransformer(
+		targetClass = "mffs/BlockForceField",
 		targetMethodName = "a",
 		targetMethodDescription = "(Lnet/minecraft/server/World;IIILnet/minecraft/server/Entity;)V"
 )
@@ -26,8 +53,16 @@ public class BlockForceFieldTransformer extends MethodNode {
 	private void visitWorldIsStatic(MethodVisitor methodVisitor) {
 		IfStatementBuilder worldIsStatic = IfStatementBuilder.newBuilder();
 
+		// ---
+		// if (world is not static) {
+		//     visitIsZapper(...)
+		// }
+
 		worldIsStatic.onLoad(loadMethodVisitor -> {
+			// par1: world
 			loadMethodVisitor.visitVarInsn(ALOAD, 1);
+
+			// is world static?
 			loadMethodVisitor.visitFieldInsn(
 					GETFIELD,
 					"net/minecraft/server/World",
@@ -46,12 +81,24 @@ public class BlockForceFieldTransformer extends MethodNode {
 	private void visitIsZapper(MethodVisitor methodVisitor) {
 		IfStatementBuilder isZapper = IfStatementBuilder.newBuilder();
 
+		// ---
+		// if (block at xyz is connected to a projector with Zapper upgrade) {
+		//     visitInstanceofLiving()
+		// }
+
 		isZapper.onLoad(loadMethodVisitor -> {
 			loadMethodVisitor.visitVarInsn(ALOAD, 1);
+
+			// par1: x coord
 			loadMethodVisitor.visitVarInsn(ILOAD, 2);
+
+			// par2: y coord
 			loadMethodVisitor.visitVarInsn(ILOAD, 3);
+
+			// par3: z coord
 			loadMethodVisitor.visitVarInsn(ILOAD, 4);
 
+			// retrieve block meta value
 			loadMethodVisitor.visitMethodInsn(
 					INVOKEVIRTUAL,
 					"net/minecraft/server/World",
@@ -59,6 +106,7 @@ public class BlockForceFieldTransformer extends MethodNode {
 					"(III)I"
 			);
 
+			// check if block at this xyz is a zapper
 			loadMethodVisitor.visitMethodInsn(
 					INVOKESTATIC,
 					"mffs/BlockForceField",
@@ -77,6 +125,11 @@ public class BlockForceFieldTransformer extends MethodNode {
 	private void visitInstanceofLiving(MethodVisitor methodVisitor) {
 		IfStatementBuilder instanceofLiving = IfStatementBuilder.newBuilder();
 
+		// ---
+		// if (entity instanceof nms/EntityLiving) {
+		//      apply damage
+		// }
+
 		instanceofLiving.onLoad(loadMethodVisitor -> {
 			loadMethodVisitor.visitVarInsn(ALOAD, 5);
 			loadMethodVisitor.visitTypeInsn(
@@ -93,42 +146,6 @@ public class BlockForceFieldTransformer extends MethodNode {
 	}
 
 	private void visitApplyEntityDamage(MethodVisitor methodVisitor) {
-//		methodVisitor.visitFieldInsn(
-//				GETSTATIC,
-//				"java/lang/System",
-//				"out",
-//				"Ljava/io/PrintStream;"
-//		);
-//		methodVisitor.visitLdcInsn("HELLO WORLD!!!!!!!!!!1");
-//		methodVisitor.visitMethodInsn(
-//				INVOKEVIRTUAL,
-//				"java/io/PrintStream",
-//				"println",
-//				"(Ljava/lang/String;)V"
-//		);
-//
-//		methodVisitor.visitInsn(RETURN);
-
-//		------------------------------------------------------------
-
-//		methodVisitor.visitVarInsn(ALOAD, 5);
-//		methodVisitor.visitFieldInsn(
-//				GETSTATIC,
-//				"net/minecraft/server/DamageSource",
-//				"GENERIC",
-//				"Lnet/minecraft/server/DamageSource;"
-//		);
-//		methodVisitor.visitInsn(ICONST_5);
-//		methodVisitor.visitMethodInsn(
-//				INVOKEVIRTUAL,
-//				"net/minecraft/server/Entity",
-//				"damageEntity",
-//				"(Lnet/minecraft/server/DamageSource;I)Z"
-//		);
-//		methodVisitor.visitInsn(POP);
-//
-//		methodVisitor.visitInsn(RETURN);
-
 
 		// --- EntityPlayer fake = new EntityPlayer(((CraftServer)Bukkit.getServer()).getServer(), var1, "[MFFS]", new ItemInWorldManager(var1));
 
@@ -176,29 +193,42 @@ public class BlockForceFieldTransformer extends MethodNode {
 		);
 		methodVisitor.visitVarInsn(ASTORE, 6);
 
-		// ---  EntityDamageByEntityEvent event = (EntityDamageByEntityEvent)CraftEventFactory.callEntityDamageEvent(fake, var5, DamageCause.CUSTOM, 5);
+		// ---
+		// EntityDamageByEntityEvent event = (EntityDamageByEntityEvent)CraftEventFactory.callEntityDamageEvent(fake, var5, DamageCause.CUSTOM, 5);
 
+		// par1: fake entityplayer
 		methodVisitor.visitVarInsn(ALOAD, 6);
+		// par2: target entity (param)
 		methodVisitor.visitVarInsn(ALOAD, 5);
+		// par3: damagecause CUSTOM
 		methodVisitor.visitFieldInsn(
 				GETSTATIC,
 				"org/bukkit/event/entity/EntityDamageEvent$DamageCause",
 				"CUSTOM",
 				"Lorg/bukkit/event/entity/EntityDamageEvent$DamageCause;"
 		);
+		// par4: initial damage amount
 		methodVisitor.visitInsn(ICONST_5);
+
+		// create damage event
 		methodVisitor.visitMethodInsn(
 				INVOKESTATIC,
 				"org/bukkit/craftbukkit/event/CraftEventFactory",
 				"callEntityDamageEvent",
 				"(Lnet/minecraft/server/Entity;Lnet/minecraft/server/Entity;Lorg/bukkit/event/entity/EntityDamageEvent$DamageCause;I)Lorg/bukkit/event/entity/EntityDamageEvent;"
 		);
+
+		// cast it to entitydamagebyentityevent
 		methodVisitor.visitTypeInsn(CHECKCAST, "org/bukkit/event/entity/EntityDamageByEntityEvent");
+
+		// store it in reg 7
 		methodVisitor.visitVarInsn(ASTORE, 7);
 
 		// ---
+		// RET before exiting IF statement to save a frame reset:
 		// if (!event.isCancelled()) {
 		//     var5.damageEntity(DamageSource.GENERIC, event.getDamage());
+		//     return;
 		// } else {
 		//     return;
 		// }
@@ -206,6 +236,7 @@ public class BlockForceFieldTransformer extends MethodNode {
 		IfStatementBuilder ifIsCancelled = IfStatementBuilder.newBuilder();
 
 		ifIsCancelled.onLoad(loadMethodVisitor -> {
+			// ifne par1: bool whether event is cancelled
 			loadMethodVisitor.visitVarInsn(ALOAD, 7);
 			loadMethodVisitor.visitMethodInsn(
 					INVOKEVIRTUAL,
@@ -217,47 +248,39 @@ public class BlockForceFieldTransformer extends MethodNode {
 		ifIsCancelled.setOpcode(IFNE);
 
 		ifIsCancelled.onTrue(trueMethodVisitor -> {
-//			trueMethodVisitor.visitVarInsn(ALOAD, 5);
-//			trueMethodVisitor.visitFieldInsn(
-//					GETSTATIC,
-//					"net/minecraft/server/DamageSource",
-//					"GENERIC",
-//					"Lnet/minecraft/server/DamageSource;"
-//			);
-//			trueMethodVisitor.visitVarInsn(ALOAD, 7);
-//			trueMethodVisitor.visitMethodInsn(
-//					INVOKEVIRTUAL,
-//					"org/bukkit/event/entity/EntityDamageByEntityEvent",
-//					"getDamage",
-//					"()I"
-//			);
+			// par1: damage source
+			trueMethodVisitor.visitVarInsn(ALOAD, 5);
+			trueMethodVisitor.visitFieldInsn(
+					GETSTATIC,
+					"net/minecraft/server/DamageSource",
+					"GENERIC",
+					"Lnet/minecraft/server/DamageSource;"
+			);
 
-			trueMethodVisitor.visitInsn(ICONST_1);
+			// par2: damage amount
+			trueMethodVisitor.visitVarInsn(ALOAD, 7);
+			trueMethodVisitor.visitMethodInsn(
+					INVOKEVIRTUAL,
+					"org/bukkit/event/entity/EntityDamageByEntityEvent",
+					"getDamage",
+					"()I"
+			);
+
+			// call damage entity
 			trueMethodVisitor.visitMethodInsn(
 					INVOKEVIRTUAL,
 					"net/minecraft/server/Entity",
 					"damageEntity",
 					"(Lnet/minecraft/server/DamageSource;I)Z"
 			);
+			// ignore call result
 			trueMethodVisitor.visitInsn(POP);
 
-//			trueMethodVisitor.visitFieldInsn(
-//					GETSTATIC,
-//					"java/lang/System",
-//					"out",
-//					"Ljava/io/PrintStream;"
-//			);
-//			trueMethodVisitor.visitInsn(SWAP);
-//			trueMethodVisitor.visitMethodInsn(
-//					INVOKEVIRTUAL,
-//					"java/io/PrintStream",
-//					"println",
-//					"(Ljava/lang/String;)V"
-//			);
-
+			// ret
 			trueMethodVisitor.visitInsn(RETURN);
 		});
 
+		// instant ret
 		ifIsCancelled.onFalse(falseMethodVisitor -> falseMethodVisitor.visitInsn(RETURN));
 		ifIsCancelled.insert(methodVisitor);
 	}
